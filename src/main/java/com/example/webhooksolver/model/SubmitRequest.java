d807ffa56fbebf2d2e8a8e1df54ea52d3797d93d
Aditya Kumar <142095991+Skepiduck87@users.noreply.github.com>
2025-09-22 15:27:37 +0530

    Add files via upload
    here it is

---
+++ src/main/java/com/example/webhooksolver/model/SubmitRequest.java
@@ -0,0 +1,1 @@
+package com.example.webhooksolver.model; public class SubmitRequest { private String finalQuery; public SubmitRequest() {} public SubmitRequest(String finalQuery) { this.finalQuery = finalQuery; } public String getFinalQuery() { return finalQuery; } public void setFinalQuery(String finalQuery) { this.finalQuery = finalQuery; } }
